ad558bff3f43efb886ac5589ddd54df419321726
Syed Usman Arif <47153131+usmanarif24@users.noreply.github.com>
2024-05-28 16:42:16 +0400

    Update

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Elementz Group-Jet Aviation-0.1";
+const contentToCache = [
+    "Build/PWA v1.loader.js",
+    "Build/PWA v1.framework.js",
+    "Build/PWA v1.data",
+    "Build/PWA v1.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
